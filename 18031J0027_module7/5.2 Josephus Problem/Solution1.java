214bda026234829a707defdff9b0de151ac3146f
RajeshReddy2306 <43745320+RajeshReddy2306@users.noreply.github.com>
2018-10-12 12:17:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 18031J0027_module7/5.2 Josephus Problem/Solution1.java
+++ 18031J0027_module7/5.2 Josephus Problem/Solution1.java
@@ -1,118 +1,89 @@
-import java.io.*;
 import java.util.Scanner;
-class Node
-{
-    int value;
-    Node link;
-}
 
-class Stack
+class Node1
 {
-    Node top;
-    public Stack()
-    {
-        top = null;
-    }
-    public void push(int item) 
-    {
-        Node n = new Node();
-        n.value = item;
-        n.link = top;
-        top = n;
-    }
-    public int pop()
-    {
-       int item;
-        item = top.value;
-        Node n = top;
-        n = null;
-        top = top.link;
-        return item;
-    }
-    public void display()
-    {
-        Node n = top;
-        System.out.print("(top)");
-        while (n != null) {
-            System.out.print(" ->" + n.value);
-            n = n.link;
-        }
-        System.out.println();
-    }
+	Object data;
+	Node1 next;
+	Node1(int data)
+	{
+		this.data=data;
+		next=null;
+	}
 }
-
-class Queue 
+class Queue
 {
-    Node front, rear;
-    Stack s1 = new Stack();
-    Stack s2 = new Stack();
-    public Queue() {
-    }
-    public void add(int item)
-    {
-        while (s2.top != null)
-            s1.push(s2.pop());
-        s1.push(item);
-        rear = s1.top;
-        while (s1.top != null)
-            s2.push(s1.pop());
-        front = s2.top;
-    }
-    public int remove()
-    {
-        int item = s2.pop();
-        front = s2.top;
-        return item;
-    }
-    public void display()
-    {
-        Node n = s2.top;
-        System.out.print("(front)");
-        while (n != null) {
-            System.out.print(" <-" + n.value);
-            n = n.link;
-        }
-        System.out.println(" <-(rear)");
-    }
+	Node1 head;
+	Node1 tail;
+	boolean isempty()
+	{
+		return head==null;
+	}
+	void add(int data)
+	{
+		Node1 n=new Node1(data);
+		if(isempty())
+		{
+			head=n;
+			tail=n;
+		}
+		else
+		{
+			tail.next=n;
+			tail=tail.next;
+		}
+				
+	}
+	int remove()
+	{
+		Node1 temp=head;
+		
+		if(isempty())
+		{
+			System.out.println("No elements");
+		}
+		else
+		{
+		head=temp.next;
+		}
+		return (Integer) temp.data;
+	}
+	
 }
 
 
 public class Solution1
 {
-    public static void main(String[] args) //throws IOException
-    {
-        
-        int n, m, i;
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        while(sc.hasNext())
-        {
-        Queue q = new Queue();
-        Queue q1 = new Queue();
-        String a=sc.nextLine();
-    	int g=0;
-    	String[] b=a.split(" ");
-    	n=Integer.parseInt(b[0]);
-    	m=Integer.parseInt(b[1]);
-        for (i = 0; i < n; i++)
-            q.add(i);
-        Node node = q.front;
-        int l, k = 0;
-        while (k != n - 1) {
-            for (i = 0; i< m-1; i++)
-                q.add(q.remove());
-            l = q.remove();
-            q1.add(l);
-            k++;
-        }
-        
-        	while (q1.front != null)
-            System.out.print(q1.remove() + " ");
-        	l=q.remove();
-            q1.add(l);
-            System.out.print(q1.remove());
-            System.out.println("");
-    }
-        
-    }
+
+	 public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in); 
+		 int k=sc.nextInt();
+		 int i=0;
+		 while(sc.hasNext())
+		 {
+	        int m = sc.nextInt();
+	        int n= sc.nextInt();
+
+	        // initialize the queue
+	        Queue queue =  new Queue(); 
+	        for (i = 0; i < m; i++)
+	        {
+	            queue.add(i);
+	        }
+
+	      //  while (!queue.isempty()) 
+		for (i = 0; i < m-1; i++)
+	        {
+	            for (int j = 0; j < n-1; j++) 
+	            {
+	                queue.add(queue.remove());
+	            }
+	            System.out.print(queue.remove()+" ");
+	        } 
+	queue.add(queue.remove());
+	        System.out.print(queue.remove());
+	        System.out.println();
+	        k++;
+		 }
+	    }
+
 }
